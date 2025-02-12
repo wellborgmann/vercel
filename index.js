6ae1388f38117259b7b1a8b62e9a6fd4b2659ca7
wellborgmann <40129426+wellborgmann@users.noreply.github.com>
2025-02-11 23:31:00 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
 import express, { query } from 'express';
 const app = express()
 const PORT = 8000
+
+import pkg from 'whatsapp-web.js';
+const { Client, LocalAuth } = pkg;
+
+
+const client = new Client({
+    authStrategy: new LocalAuth({
+        clientId: "client-2",
+        dataPath: "./session",
+    }),
+    puppeteer: {
+        args: ["--no-sandbox", "--disable-setuid-sandbox"],
+        executablePath: '/usr/bin/google-chrome',
+
+
+    },
+});
+
+client.on("qr", (qr) => {
+    qrcode.toDataURL(qr, (err, url) => {
+        if (err) {
+            console.error("Erro ao gerar QR Code:", err);
+            return;
+        }
+        imgQr = url;
+        // Emite a URL do QR Code para o frontend via WebSocket
+        io.emit("qr", url);
+    });
+});
+
+
 app.get('/', (req, res) => {
   res.send('Api wellborgmann')
 })
