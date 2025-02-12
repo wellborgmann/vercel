5c0a6dd92e37820468cf6c76343889398466ed3d
wellborgmann <40129426+wellborgmann@users.noreply.github.com>
2025-02-11 23:29:20 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express, { query } from 'express';
 const app = express()
 const PORT = 8000
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  res.send('Api wellborgmann')
 })
 app.get('/about', (req, res) => {
   res.send('About route 🎉 ')
