299e185328f46b8ee6d907476e3e823cf7f1df67
wellborgmann <40129426+wellborgmann@users.noreply.github.com>
2025-02-11 23:53:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,12 @@
 import express, { query } from 'express';
 const app = express()
 const PORT = 8000
-import { Client } from "ssh2";
-
-const connSettings = {
-    host: "157.254.54.234",
-    port: 22,
-    username: "root",
-    password: "7093dado7093",
-    readyTimeout: 30000,
-  };
 
 
-  async function checkLoginExists(loginName) {
-    let comando = `chage -l ${loginName} | grep -E 'Account expires' | cut -d ' ' -f3-`;
 
-    try {
-        const dataReceived = await executeSSHCommand(comando);
-        return {
-            exists: !!dataReceived, // Se houver dados, o usuário existe
-            data: dataReceived || null
-        };
-    } catch (error) {
-        console.error("Erro ao verificar login:", error);
-        return { exists: false };
-    }
-}
 
 app.get('/',async (req, res) => {
-  const login = req.query.login;
-    const {data} = await checkLoginExists(login);
-    res.send(data);
+res.send("ola mano");
 })
 app.get('/about', (req, res) => {
   res.send('About route 🎉 ')
