86013341500bd1a12c05503b9cf9eb276585fabb
wellborgmann <40129426+wellborgmann@users.noreply.github.com>
2025-02-13 04:14:05 -0300

    Update index.js

--- index.js
+++ index.js
@@ -56,6 +56,22 @@ async function checkLoginExists(loginName) {
     };
 }
 
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  next();
+});
+
 // Rota principal
 app.get('/', async (req, res) => {
     try {
@@ -193,20 +209,7 @@ app.get("/proxy", async (req, res) => {
 });
 // Exemplo de uso
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
 
-  next();
-});
 
 // Middleware para verificar se a URL foi fornecida
 // api/download.js
