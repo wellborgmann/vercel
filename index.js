eda84f467af1fae0a6c4939e75e5f0d0667bbb0d
wellborgmann <40129426+wellborgmann@users.noreply.github.com>
2025-02-13 02:23:53 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,45 +1,12 @@
-import { Client } from 'whatsapp-web.js';
-import * as puppeteer from 'puppeteer';
-import chromium from '@sparticuz/chromium';
-import puppeteerCore from 'puppeteer-core';
+import { WAConnection } from '@adiwajshing/baileys';
+import fs from 'fs';
 
-// Configurações do Puppeteer
-async function getPuppeteerConfig() {
-    if (process.env.NODE_ENV === 'development') {
-        console.log('Rodando em desenvolvimento...');
-        return {
-            executablePath: await puppeteer.executablePath(),
-            args: ['--no-sandbox', '--disable-setuid-sandbox'],
-            headless: true,
-        };
-    }
+const conn = new WAConnection();
 
-    if (process.env.NODE_ENV === 'production') {
-        console.log('Rodando em produção...');
-        return {
-            args: chromium.args,
-            defaultViewport: chromium.defaultViewport,
-            executablePath: await chromium.executablePath(),
-            headless: chromium.headless,
-        };
-    }
-}
+conn.on('open', () => {
+    console.log('Credentials updated!');
+    const authInfo = conn.base64EncodedAuthInfo();
+    fs.writeFileSync('./auth_info.json', JSON.stringify(authInfo, null, '\t'));
+});
 
-(async () => {
-    const puppeteerOptions = await getPuppeteerConfig();
-
-    const client = new Client({
-        puppeteer: puppeteerCore, // Usa puppeteer-core
-        puppeteerOptions, // Configurações definidas acima
-    });
-
-    client.on('qr', qr => {
-        console.log('QR CODE:', qr);
-    });
-
-    client.on('ready', () => {
-        console.log('WhatsApp Web está pronto!');
-    });
-
-    await client.initialize();
-})();
+conn.connect();
