1d33e0d42ac9d22a0365fb413ec3401aa92ea1c9
wellborgmann <40129426+wellborgmann@users.noreply.github.com>
2025-02-13 03:59:42 -0300

    Update index.js

--- index.js
+++ index.js
@@ -193,7 +193,20 @@ app.get("/proxy", async (req, res) => {
 });
 // Exemplo de uso
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
 
+  next();
+});
 
 app.get('/download', async (req, res) => {
   const targetUrl = req.query.url;
